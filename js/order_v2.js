68c309b03514e8cffb831e20b5d6d70a9f94485a
movingroovin <finnbloo@gmail.com>
2021-08-24 00:26:09 +0800

    order process version 2

---
+++ js/order_v2.js
@@ -0,0 +1,27 @@
+const vueInstance = Vue.createApp({
+  data() {
+    return {
+      orderStep: {
+        isStep0: true,
+        isStep1: false,
+        isStep2: false,
+        isStep3: false,
+        isStep4: false,
+        isStep5: false,
+        isStep6: false,
+        isStep7: false,
+      },
+    }
+  },
+  created() {},
+  methods: {
+    ToStep(toStep) {
+      for (const key in this.orderStep) {
+        this.orderStep[key] = false;
+      }
+      this.orderStep[`isStep${toStep}`] = true;
+    },
+  },
+
+});
+const app = vueInstance.mount("#app");
